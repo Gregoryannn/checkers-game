f7e57bd206a9c25d858914b8bec6411599b6800b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-03-30 09:35:53 +0100

    improvements

--- src/js/Main.js
+++ src/js/Main.js
@@ -2,24 +2,31 @@
 
     console.log('checkers game running...');
 
-    var settings = {
-        scope: 'game-stage',
-        board: 'board',
-        pieces: 'pieces'
+    var app = {},
+        settings = {
+            scope: 'game-stage',
+            board: 'board',
+            pieces: 'pieces'
+        };
+
+    app.init = function() {
+        app.setup();
+        app.create();
+        app.attach();
+        app.bind();
+
+
     };
 
-    var scope = document.getElementById(settings.scope),
-        board = document.getElementsByClassName(settings.board)[0],
-        pieces = document.getElementsByClassName(settings.pieces)[0];
-
-
-    init = function() {
-        create();
-        attach();
-        bind();
+    app.setup = function() {
+        app.scope = document.getElementById(settings.scope),
+            app.board = document.getElementsByClassName(settings.board)[0],
+            app.pieces = document.getElementsByClassName(settings.pieces)[0];
     };
 
-    create = function() {
+
+
+    app.create = function() {
         var html = [];
 
         for (var i = 0; i < 8; i++) {
@@ -31,11 +38,11 @@
                 html.push('<span class="board__square ' + squareType + '">');
 
                 if (squareType === 'dark' && i < 3) {
-                    html.push('<span class="board__piece" draggable="true">' + i + '-' + j + '</span>');
+                    html.push('<span class="board__piece dark" data-position="' + i + '-' + j + '">' + i + '-' + j + '</span>');
                 }
 
                 if (squareType === 'dark' && i > (8 - 4)) {
-                    html.push('<span class="board__piece" draggable="true">' + i + '-' + j + '</span>');
+                    html.push('<span class="board__piece light" data-position="' + i + '-' + j + '">' + i + '-' + j + '</span>');
                 }
 
                 html.push('</span>');
@@ -44,19 +51,34 @@
             html.push('</div>');
         }
 
-        board.innerHTML = html.join('');
+        app.board.innerHTML = html.join('');
     };
 
-    attach = function() {
+    app.attach = function() {
+        app.pieces = document.getElementsByClassName('board__piece');
+    };
+
+    app.bind = function() {
+        for (var i = 0, len = app.pieces.length; i < len; i++) {
+            app.pieces[i].addEventListener('click', function() {
+                app.move(this);
+            }, false);
+        }
 
     };
 
-    bind = function() {
+    app.move = function(target) {
+        var dataPos = target.getAttribute('data-position').split('-'),
+            position = {
+                col: dataPos[0],
+                line: dataPos[1]
+            };
+
+        console.log(position.col, position.line);
 
     };
 
 
-
-    init();
+    app.init();
 
 })();
